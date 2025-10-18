f5262d40e23da6074dd4d6401ac0a3c93a647b30
chris <cbailey111999@icloud.com>
2025-10-17 22:40:57 -0400

    add more notes

---
+++ JaveScript/TernaryOper.js
@@ -0,0 +1,9 @@
+//let number = 7;
+//if(number === 7) {
+    //console.log(`Lucky`);
+//}
+//else {
+  //  console.log(`Unlucky`);
+//}
+
+//number === 7 ? console.log(`Lucky`) : console.log(`Unlucky`);
